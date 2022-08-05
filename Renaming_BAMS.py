ad4e904551f1937304159eec4ff7b2c7ecf1ce87
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2022-08-05 15:07:16 +0200

    Add files via upload

--- Renaming_BAMS.py
+++ Renaming_BAMS.py
@@ -3,7 +3,7 @@ import os.path
 
 
 
-BAM_TO_NAME={}
+SAMPLE_TO_SPECIES={}
 
 if os.path.exists('METADATA'):
     META_DATA_FILE=open('METADATA','r')
@@ -15,46 +15,25 @@ if os.path.exists('METADATA'):
 
     #### Read METADATA file, create Dictionary
     
-    SPECIES_COUNT={}
 
     
     for LINE in META_DATA_FILE:
     
 
         LINE=LINE.strip().split('\t')
-        SAMPLE=LINE[1]
+        SAMPLE_NAME=LINE[1]
         SPECIES='_'.join(LINE[5].split(' '))
-        NAME=SPECIES
-        
-        
-        if SAMPLE not in BAM_TO_NAME.keys():
-            if NAME not in SPECIES_COUNT.keys():
-                SPECIES_COUNT[NAME]=1
-            else:
-                SPECIES_COUNT[NAME]+=1
-        
-            print(NAME,SAMPLE)
-                
-
-        BAM_TO_NAME[SAMPLE]=NAME
+        SAMPLE_TO_SPECIES[SAMPLE_NAME]=SPECIES
 
 
-print(BAM_TO_NAME)
+
+print(SAMPLE_TO_SPECIES)
 print('\n')
-print(SPECIES_COUNT)
 
 
-for J in SPECIES_COUNT.keys():
-    counter=1
-    for K in BAM_TO_NAME.keys():
-        
-        if BAM_TO_NAME[K]==J:
-            BAM_TO_NAME[K]=BAM_TO_NAME[K]+'_'+str(counter)
-            counter+=1
 
-print(BAM_TO_NAME)
 
-for V in BAM_TO_NAME:
+for V,J in SAMPLE_TO_SPECIES.items():
     
-    os.system(f'mv FINISHED/{V}.bam FINISHED/{V}_{BAM_TO_NAME[V]}.bam')
-    os.system(f'mv FINISHED/{V}.bam.bai FINISHED/{V}_{BAM_TO_NAME[V]}.bam.bai')
+    os.system(f'cp {V}.bam FINISHED/{V}_{J}.bam')
+    os.system(f'cp {V}.bam.bai FINISHED/{V}_{J}.bam.bai')
